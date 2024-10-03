3e8ad14a99acfaa477e2de82c9e5fb96a6df6f9f
root <root@localhost>
2024-10-03 19:59:23 +0300

    auto publish 1727974762894

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+ imt age;
+ float income;
+ 
+ printf("enter your age");
+ scanf("%d",age);
+ 
+ printf("enter your annual income (in shillings)");
+ scanf("%f", income);
+ 
+ if(age>=21, income>=21000);
+ printf("congratulations you qualify for a loan");
+ else
+ printf("unfortunately,we are not able to offer you a loan");
+ 
+ return 0;
+}
